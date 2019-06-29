f7bbdf2c1f43bad7f4a1d50956b74ab78a150ac7
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-29 15:44:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,11 +36,22 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-
+  total = 0
+  i = starting_point
+  while i < source_array.length
+    total += source_array[i]
+  end
+  
+  total
 end
 
 def reduce_to_all_true(source_array)
-
+  total = 0
+  source_array.each do |item|
+    total += item
+  end
+  
+  total
 end
 
 def reduce_to_any_true(source_array)
