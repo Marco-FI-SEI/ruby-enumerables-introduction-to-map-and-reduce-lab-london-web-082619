30d3ac47725f210ad5fbc3e97b39849c25f02929
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-29 15:35:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,16 +9,26 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-  source_array
+  new_source_array = []
+  source_array.each do |item|
+    new_source_array.push(item)
+  end
 end
 
 def map_to_double(source_array)
-
+  new_source_array = []
+  source_array.each do |item|
+    new_source_array.push(item * 2)
+  end
 end
 
 def map_to_square(source_array)
-
+  new_source_array = []
+  source_array.each do |item|
+    new_source_array.push(item ** 2)
+  end
 end
+
 def reduce_to_total(source_array, starting_point)
 
 end
