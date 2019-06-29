79a18ce23c15e56768ca4877806d38ce0e80735d
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-29 15:36:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,6 +13,8 @@ def map_to_no_change(source_array)
   source_array.each do |item|
     new_source_array.push(item)
   end
+  
+  new_source_array
 end
 
 def map_to_double(source_array)
@@ -20,6 +22,8 @@ def map_to_double(source_array)
   source_array.each do |item|
     new_source_array.push(item * 2)
   end
+  
+  new_source_array
 end
 
 def map_to_square(source_array)
@@ -27,6 +31,8 @@ def map_to_square(source_array)
   source_array.each do |item|
     new_source_array.push(item ** 2)
   end
+  
+  new_source_array
 end
 
 def reduce_to_total(source_array, starting_point)
